d2b60a6d950dd0ae3324e3fb118ee3ca81ea0b81
Guilherme Coutinho <lgcb@ic.ufal.br>
2022-04-28 17:59:45 -0300

    Metódos sacar e depositar

--- src/Conta.php
+++ src/Conta.php
@@ -5,4 +5,18 @@ class Conta
     public string $cpf;
     public string $nomeTitular;
     public float $saldo;
+
+    public function sacar(float $valorASacar)
+    {
+        if ($valorASacar > $this->saldo) {
+            echo "Saldo indisponível";
+        } else {
+            $this->saldo -= $valorASacar;
+        }
+    }
+
+    public function depositar(float $valorADepositar)
+    {
+        $this->saldo += $valorADepositar;
+    }
 }
