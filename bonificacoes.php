c6e43ebc7b4fec6d2adbfee8713d8b342d1225aa
Guilherme Coutinho <lgcb@ic.ufal.br>
2022-05-24 18:00:54 -0300

    Adicionando classe EditorVideo

--- bonificacoes.php
+++ bonificacoes.php
@@ -3,28 +3,31 @@
 require_once 'autoload.php';
 
 use Alura\Banco\Modelo\Cpf;
-use Alura\Banco\Modelo\Funcionario\{Desenvolvedor, Diretor, Funcionario, Gerente};
+use Alura\Banco\Modelo\Funcionario\{Desenvolvedor, Diretor, EditorVideo, Gerente};
 use Alura\Banco\Service\ControladorBonificacoes;
 
 $umFuncionario = new Desenvolvedor(
     'Vinicius Dias',
     new CPF('123.456.789-10'),
-    'Desenvolvedor',
-    '1000'
+    1000
 );
 
 $umaFuncionaria = new Gerente(
     'Patricia',
     new CPF('987.654.321-10'),
-    'Gerente',
-    '3000'
+    3000
 );
 
 $umDiretor = new Diretor(
     'Ana Paula',
     new Cpf('123.545.654-11'),
-    'Diretor',
-    '5000'
+    5000
+);
+
+$umEditor = new EditorVideo(
+    'Paulo',
+    new Cpf('456.987.231-11'),
+    1500
 );
 
 $controlador = new ControladorBonificacoes();
--- src/Modelo/Funcionario/Funcionario.php
+++ src/Modelo/Funcionario/Funcionario.php
@@ -4,21 +4,14 @@ namespace Alura\Banco\Modelo\Funcionario;
 
 use Alura\Banco\Modelo\{Cpf, Pessoa};
 
-class Funcionario extends Pessoa
+abstract class Funcionario extends Pessoa
 {
-    private string $cargo;
     private float $salario;
 
-    public function __construct(string $nome, Cpf $cpf, string $cargo, float $salario)
+    public function __construct(string $nome, Cpf $cpf, float $salario)
     {
         parent::__construct($nome, $cpf);
-        $this->cargo = $cargo;
         $this->salario = $salario;
-    }
-
-    public function recuperaCargo(): string
-    {
-        return $this->cargo;
     }
 
     public function recuperaSalario(): float
@@ -42,8 +35,5 @@ class Funcionario extends Pessoa
         $this->salario += $valorAumento;
     }
 
-    public function calculaBonificacao(): float
-    {
-        return $this->salario * 0.1;
-    }
+    abstract public function calculaBonificacao();
 }
